af76563d4e4cb4b37677dc09f12e05631c477b59
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2026-02-27 16:46:55 +0530

    ..

---
+++ profil.js
@@ -0,0 +1,9 @@
+const profile = {};
+
+profile.name = "Jai Kishan";
+profile.role = "Full Stack MERN Developer";
+profile.skills = ["React", "Node.js", "MongoDB", "AWS"];
+profile.learning = ["Docker", "DevOps"];
+profile.background = "Commerce → Software Engineering";
+profile.passion = "Building Scalable Web Applications";
+profile.status = "Open to Opportunities 🚀";
